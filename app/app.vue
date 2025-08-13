68d89e2c344672c5bc4aebb37c38bbf87651db56
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2025-08-13 09:23:35 -0300

    Instalação do Nuxt
    Organização do ambiente de desenvolvimento e instalação de dependências do projeto.

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="w-screen h-screen">
+    <main>
+      <section class="bg-[url('/images/background-1.png')] bg-cover bg-center h-120">
+        <h1>
+          Sejam bem vindos ao meu site!
+        </h1>
+      </section>
+    </main>
+  </div>
+</template>
