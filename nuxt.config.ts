741d9a68c9cba8fc8a6e6d6103a3ba0913244bb4
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2025-08-19 18:40:23 -0300

    Teste de deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -11,7 +11,7 @@ export default defineNuxtConfig({
         lang: 'pt-br',
       },
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: 'public/favicon.ico' }
       ]
     }
   },
