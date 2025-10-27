72badb128325dc3416e8d9370a41dddc6632a2e2
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2025-10-27 18:35:10 -0300

    Update
    Aqui, realizo a inserção de mais uma seção, apenas o html, sem estilização.

--- app/components/AbaCards.vue
+++ app/components/AbaCards.vue
@@ -1,34 +1,41 @@
 <template>
-    <div  v-for="(description, title) in cards" :key="description.title" class="">
-        <ul>
-            <li>
-                <h3 class="">
-                    {{ cards[title].title }}
-                </h3>
-            </li>
-        </ul>
+  <div v-for="(card, index) in cards" :key="card.title" class="">
+    <ul>
+      <li class="flex flex-row justify-between">
+        <h3 class="">
+          {{ card.title }}
+        </h3>
+        <img :src="card.image" :alt="card.title" />
+      </li>
+    </ul>
 
-        <p class="">
-            {{ description.description }}
-        </p>
-    </div>
+    <p class="">
+      {{ card.description }}
+    </p>
+  </div>
 </template>
 
 <script setup>
-    import { ref } from 'vue';
+import { ref } from 'vue';
+import Foguete from '../assets/svg/Foguete.svg';
+import QuebraCabecas from '../assets/svg/Quebra_cabecas.svg';
+import Balao from '../assets/svg/Balao.svg';
 
-    const cards = ref([
-        {
-            title: 'Card 1',
-            description: 'Descrição do card 1',
-        },
-        {
-            title: 'Card 2',
-            description: 'Descrição do card 2',
-        },
-        {
-            title: 'Card 3',
-            description: 'Descrição do card 3',
-        }
-    ])
+const cards = ref([
+  {
+    title: 'Card 1',
+    description: 'Descrição do card 1',
+    image: Foguete
+  },
+  {
+    title: 'Card 2',
+    description: 'Descrição do card 2',
+    image: QuebraCabecas
+  },
+  {
+    title: 'Card 3',
+    description: 'Descrição do card 3',
+    image: Balao
+  }
+]);
 </script>
