46288b4b5a7c746ee9b57f6d4d3e51a76cba1b2d
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2025-10-29 14:36:39 -0300

    Update
    Mudanças estéticas na última seção adicionada no site.

--- app/components/AbaCards.vue
+++ app/components/AbaCards.vue
@@ -1,41 +1,53 @@
 <template>
-  <div v-for="(card, index) in cards" :key="card.title" class="">
-    <ul>
-      <li class="flex flex-row justify-between">
-        <h3 class="">
-          {{ card.title }}
-        </h3>
-        <img :src="card.image" :alt="card.title" />
-      </li>
-    </ul>
-
-    <p class="">
-      {{ card.description }}
-    </p>
+  <div class="grid gap-4 lg:gap-10  lg:grid-cols-2 lg:p-10">
+    <div v-for="(card) in cards" :key="card.title" :class="` gap-10 text-white ${card.bgColor} p-6`">
+      <div>
+        <ul>
+          <li class="flex flex-row justify-between">
+            <h3 class="font-bold text-xl">
+              {{ card.title }}
+            </h3>
+            <img :src="card.image" :alt="card.title" class="size-12" />
+          </li>
+        </ul>
+        <p class="text-sm">
+          {{ card.description }}
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-import Foguete from '../assets/svg/Foguete.svg';
-import QuebraCabecas from '../assets/svg/Quebra_cabecas.svg';
-import Balao from '../assets/svg/Balao.svg';
+import Foguete from '../assets/svg/Foguete.svg?url';
+import QuebraCabecas from '../assets/svg/Quebra_cabecas.svg?url';
+import Balao from '../assets/svg/Balao.svg?url';
 
 const cards = ref([
   {
-    title: 'Card 1',
-    description: 'Descrição do card 1',
+    bgColor: 'bg-[var(--blue)]',
+    title: '1. Desenvolvimento de habilidades sociais e de comunicação',
+    description: 'A ABA ajuda a ensinar formas eficazes de se comunicar, seja por meio da fala, gestos ou outras formas alternativas, promovendo maior interação com o meio e com outras pessoas.',
     image: Foguete
   },
   {
-    title: 'Card 2',
-    description: 'Descrição do card 2',
+    bgColor: 'bg-[var(--dark-green)]',
+    title: '2. Redução de comportamentos desafiadores',
+    description: 'Com técnicas baseadas em reforço positivo, a terapia ABA busca reduzir comportamentos que atrapalham o aprendizado ou a convivência social, como agressividade, crises ou estereotipias.',
     image: QuebraCabecas
   },
   {
-    title: 'Card 3',
-    description: 'Descrição do card 3',
+    bgColor: 'bg-[var(--red)]',
+    title: '3. Promoção da autonomia e independência',
+    description: 'A ABA ensina habilidades adaptativas do dia a dia, como se vestir, escovar os dentes, organizar objetos ou seguir rotinas, o que aumenta a independência da criança em diferentes contextos.',
     image: Balao
+  },
+  {
+    bgColor: 'bg-[var(--pink)]',
+    title: '4. Melhora do desempenho acadêmico',
+    description: 'Através de estratégias individualizadas, a ABA pode ajudar a criança a desenvolver habilidades acadêmicas, como leitura, escrita e matemática, facilitando seu progresso escolar.',
+    image: Foguete
   }
 ]);
 </script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -215,17 +215,16 @@
           <Convenios />
         </div>
       </section>
-      <section>
-        <p>
+      <section class="px-5 md:px-10 relative my-10">
+        <p class="text-[var(--blue)]">
           Mais do que tratar, buscamos compreender e acompanhar cada etapa do desenvolvimento de seu filho.
         </p>
-
-        <h2>
+        <h2 class="text-xl md:text-2xl xl:text-3xl font-semibold">
           Benefícios da Terapia ABA
         </h2>
-        <div id="separador" class="h-0.5 w-60 right-0 content-[''] absolute [background:linear-gradient(to_right,_#206FBD_0%_16.666%,_#05528D_16.666%_33.333%,_#597E22_33.333%_50%,_#87BF34_50%_66.666%,_#FF89DD_66.666%_83.333%,_#F266C1_83.333%_100%)]"></div>
+        <div id="separador" class="h-0.5 w-60 left-10 absolute [background:linear-gradient(to_right,_#206FBD_0%_16.666%,_#05528D_16.666%_33.333%,_#597E22_33.333%_50%,_#87BF34_50%_66.666%,_#FF89DD_66.666%_83.333%,_#F266C1_83.333%_100%)]"></div>
 
-        <AbaCards />
+        <AbaCards class="my-6" />
       </section>
     </main>
   </div>
